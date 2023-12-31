86762b07f53e57ab470c1a04ed132fd2868d9577
Ecastro08 <worldmechatronics2023@gmail.com>
2023-12-31 17:30:31 -0500

    clase 31/12/2023

---
+++ arrays/creacion.js
@@ -0,0 +1,20 @@
+// creacion arrays
+new Array()
+
+Array()
+
+const fruits = Array("apple", "banana", "orange")
+
+// array con literal syntaxis
+
+const oneNumber = [4]
+
+// acceder a los arrays
+
+const firstFruit = fruits[0]
+
+// el tamaño del array
+
+fruits.length
+
+
